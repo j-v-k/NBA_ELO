984b33fc62f783afb44e33094ff09838e52ad6b9
Jvonkaenel <jameshvk@gmail.com>
2017-09-05 23:58:06 -0700

    added DB boilerplate

--- Postgress_Test.py
+++ Postgress_Test.py
@@ -8,6 +8,6 @@ import psycopg2
 def connect():
     try:
         conn = psycopg2.connect("dbname='dcfvunds3mi9ke' user='xkfuywemblzisn' host='ec2-50-19-105-113.compute-1.amazonaws.com' password='627a78ad25ed73505ba7af0dbb28eeefb18acb4d6a2c2fd10fe6498c13d99cc9'")
-        print "connect"
+        print("connect")
     except:
-        print "I am unable to connect to the database"
+        print("I am unable to connect to the database")
