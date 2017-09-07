0159edadf4e7cd7b1a792cf3ae25998e755abcdd
Jvonkaenel <jameshvk@gmail.com>
2017-09-06 22:33:14 -0700

    b

--- app.py
+++ app.py
@@ -15,7 +15,7 @@ import os
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = os.environ['DATABASE_URL']
-db = SQLAlchemy(app)
+cnxn = psycopg2.connect("dbname='dcfvunds3mi9ke' user='xkfuywemblzisn' host='ec2-50-19-105-113.compute-1.amazonaws.com' password='627a78ad25ed73505ba7af0dbb28eeefb18acb4d6a2c2fd10fe6498c13d99cc9'") 
 api = Api(app)
 #cnxn = sqlalchemy.create_engine('mssql+pyodbc:///?odbc_connect={}'.format(quoted))
 
@@ -23,7 +23,6 @@ class Players(Resource):
     def get(self):
         #conn = db_connect.connect() # connect to database
         #cnxn = pyodbc.connect('DRIVER={MySQL ODBC 5.3 ANSI Driver};SERVER=localhost;PORT=3306;DATABASE=nba_ranking_app;UID=root;PASSWORD=tintin;')
-        quoted = urllib.parse.quote('DRIVER={MySQL ODBC 5.3 ANSI Driver};Server=localhost;Database=nba_ranking_app;UID=root;PWD=tintin;TDS_Version=8.0;Port=3306;')
         
 
         query = cnxn.execute("select * from Players") # This line performs query and returns json result
@@ -34,7 +33,6 @@ class Players_Name(Resource):
         #conn = db_connect.connect()
         #cnxn = pyodbc.connect('DRIVER={MySQL ODBC 5.3 ANSI Driver};SERVER=localhost;PORT=3306;DATABASE=nba_ranking_app;UID=root;PASSWORD=tintin;')
         
-        quoted = urllib.parse.quote('DRIVER={MySQL ODBC 5.3 ANSI Driver};Server=localhost;Database=nba_ranking_app;UID=root;PWD=tintin;TDS_Version=8.0;Port=3306;')
         
         query = cnxn.execute("select * from Players where ID =%d "  %int(player_id))
         #file = open(r'C:\Users\James\Documents\GitHub\python_rest\testfile2.txt', 'w')
@@ -51,7 +49,6 @@ class User_Info(Resource):
     def get(self):
         #conn = db_connect.connect() # connect to database
         #cnxn = pyodbc.connect('DRIVER={MySQL ODBC 5.3 ANSI Driver};SERVER=localhost;PORT=3306;DATABASE=nba_ranking_app;UID=root;PASSWORD=tintin;')
-        quoted = urllib.parse.quote('DRIVER={MySQL ODBC 5.3 ANSI Driver};Server=localhost;Database=nba_ranking_app;UID=root;PWD=tintin;TDS_Version=8.0;Port=3306;')
         
         query = cnxn.execute("select * from player_chosen") # This line performs query and returns json result
         return {'players': [query.fetchall()]}
@@ -69,7 +66,7 @@ class User_Info(Resource):
         data_dict = {i: request.json[i] for i in request.json.keys() if request.json[i] != ''}
        
         """gets creates the query to insert into the user info based on the json provided from the post""" 
-        qstring =  "INSERT INTO user_info ("
+        qstring =  "INSERT INTO user_choice ("
         qstring += ", ".join(list(data_dict.keys())) + ") VALUES ('" + "','".join(list(data_dict.values())) + "')"
         query = cnxn.execute(qstring) # This line performs query and returns json result
         print("Choice Sent")
@@ -81,14 +78,12 @@ class Players_Respone(Resource):
     def get(self):
         #conn = db_connect.connect() # connect to database
         #cnxn = pyodbc.connect('DRIVER={MySQL ODBC 5.3 ANSI Driver};SERVER=localhost;PORT=3306;DATABASE=nba_ranking_app;UID=root;PASSWORD=tintin;')
-        quoted = urllib.parse.quote('DRIVER={MySQL ODBC 5.3 ANSI Driver};Server=localhost;Database=nba_ranking_app;UID=root;PWD=tintin;TDS_Version=8.0;Port=3306;')
         
         query = cnxn.execute("select * from player_chosen") # This line performs query and returns json result
         return {'players': [query.fetchall()]}
     
     def post(self):
         #cnxn = pyodbc.connect('DRIVER={MySQL ODBC 5.3 ANSI Driver};SERVER=localhost;PORT=3306;DATABASE=nba_ranking_app;UID=root;PASSWORD=tintin;')
-        quoted = urllib.parse.quote('DRIVER={MySQL ODBC 5.3 ANSI Driver};Server=localhost;Database=nba_ranking_app;UID=root;PWD=tintin;TDS_Version=8.0;Port=3306;')
         cnxn = sqlalchemy.create_engine('mssql+pyodbc:///?odbc_connect={}'.format(quoted))
         
         
@@ -98,7 +93,7 @@ class Players_Respone(Resource):
         Player_2 = request.json['Player_2']
         Player_Choice = request.json['Player_Choice']
         TimeEntered = request.json['TimeEntered']
-        query = cnxn.execute("INSERT INTO user_choice (User_ID, Player_1, Player_2, Player_Choice, TimeEntered) VALUES ('%s','%s','%s','%s', '%s')" % (str(User_ID), Player_1,Player_2, Player_Choice, TimeEntered)) # This line performs query and returns json result
+        query = cnxn.execute("INSERT INTO player_choice (User_ID, Player_1, Player_2, Player_Choice, TimeEntered) VALUES ('%s','%s','%s','%s', '%s')" % (str(User_ID), Player_1,Player_2, Player_Choice, TimeEntered)) # This line performs query and returns json result
         print("Choice Sent")
         
         return {'status':'success','data': request.json}  
