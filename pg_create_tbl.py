83ba78dfd3361e6f01fa01c3f1427525efa54573
Jvonkaenel <jameshvk@gmail.com>
2017-09-06 20:46:04 -0700

    m

--- pg_create_tbl.py
+++ pg_create_tbl.py
@@ -6,23 +6,18 @@
 import psycopg2
 
 def main():
-	conn = psycopg2.connect("dbname='dcfvunds3mi9ke' user='xkfuywemblzisn' host='ec2-50-19-105-113.compute-1.amazonaws.com' password='627a78ad25ed73505ba7af0dbb28eeefb18acb4d6a2c2fd10fe6498c13d99cc9'")
-	cursor = conn.cursor()
-	print("connect")
-	sql_query = """CREATE TABLE user_choice (
-        Gender varchar(40) NULL,
-        Age varchar(40) NULL,
-        Country varchar(40) NULL,
-        FavTeam varchar(40) NULL,
-        TimeEntered varchar(30) NULL
-        );"""
-	cursor.execute(sql_query) 
-	cursor.commit()
+        conn = psycopg2.connect("dbname='dcfvunds3mi9ke' user='xkfuywemblzisn' host='ec2-50-19-105-113.compute-1.amazonaws.com' password='627a78ad25ed73505ba7af0dbb28eeefb18acb4d6a2c2fd10fe6498c13d99cc9'")
+        conn.autocommit = True
+        cursor = conn.cursor()
+        print("connect")
+        sql_query = """CREATE TABLE user_choice (Gender varchar(40) NULL,Age varchar(40) NULL,Country varchar(40) NULL,FavTeam varchar(40) NULL,TimeEntered varchar(30) NULL);"""
+        cursor.execute(sql_query) 
+        #cursor.commit()
         cursor.execute("SELECT * FROM user_choice")
- 
-	# retrieve the records from the database
-	records = cursor.fetchall()
-	print records
+
+        # retrieve the records from the database
+        records = cursor.fetchall()
+        print records
 	
 if __name__ == "__main__":
 	main()
