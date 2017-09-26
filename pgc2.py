6556d460bb2e35fffe60876f578fa8fb53ba5e30
Jvonkaenel <jameshvk@gmail.com>
2017-09-25 23:30:33 -0700

    commit

--- pgc2.py
+++ pgc2.py
@@ -1,8 +1,8 @@
 import psycopg2
 
 def main():
-	print("running")
-        conn = psycopg2.connect("dbname='dcfvunds3mi9ke' user='xkfuywemblzisn' host='ec2-50-19-105-113.compute-1.amazonaws.com' password='627a78ad25ed73505ba7af0dbb28eeefb18acb4d6a2c2fd10fe6498c13d99cc9'")
+	
+        conn = psycopg2.connect("""dbname='dcfvunds3mi9ke' user='xkfuywemblzisn' host='ec2-50-19-105-113.compute-1.amazonaws.com' password='627a78ad25ed73505ba7af0dbb28eeefb18acb4d6a2c2fd10fe6498c13d99cc9'""")
         conn.autocommit = True
         cursor = conn.cursor()
         print("connect")
