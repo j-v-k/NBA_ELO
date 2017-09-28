749fdde6c2ba6a320a801f45417568e25299d970
Jvonkaenel <jameshvk@gmail.com>
2017-09-27 23:46:15 -0700

    pgconn

--- pg_create_tbl.py
+++ pg_create_tbl.py
@@ -7,7 +7,7 @@ import psycopg2
 
 def main():
 	print("running")
-        conn = psycopg2.connect("dbname='dcfvunds3mi9ke' user='xkfuywemblzisn' host='ec2-50-19-105-113.compute-1.amazonaws.com' password='627a78ad25ed73505ba7af0dbb28eeefb18acb4d6a2c2fd10fe6498c13d99cc9'")
+        conn = psycopg2.connect("dbname='initdb' user='postgres' host='localhost' password='tintin'")
         conn.autocommit = True
         cursor = conn.cursor()
         print("connect")
